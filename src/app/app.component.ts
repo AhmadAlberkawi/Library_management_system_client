e3a5d5309c50628442dc7053bba2bc16ed423648
AhmadAlberkawi <46059053+AhmadAlberkawi@users.noreply.github.com>
2021-11-28 21:53:56 +0100

    login interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'user';
+
+export class AppComponent implements OnInit {
+
+  title = '';
+
+  loggedIn: boolean;
+
+  model: any = {};
+
+  constructor(private accountSerive: AccountService) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  login() {
+    this.accountSerive.login(this.model).subscribe(Response => {
+      console.log(Response);
+      this.loggedIn = true;
+    },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+
 }
+
+
+
+
+  /*
+
+  students: any;
+
+
+  ngOnInit(): void {
+    this.getStudents();
+  }
+
+  getStudents() {
+
+    this.http.get('https://localhost:5001/Bvs_Api/Student').subscribe(Response => {
+      this.students = Response;
+    },
+      error => {
+        console.log(error);
+      }
+    )
+  }
+  */
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderNavComponent } from './header-nav/header-nav.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderNavComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
